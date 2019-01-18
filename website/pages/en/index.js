a723175651701e6f20a34749ca296a45ef6d0d79
Michał Zieliński <michal@zielinscy.org.pl>
2019-01-18 15:35:03 +0100

    add GA, fix two links

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -151,14 +151,14 @@ class Index extends React.Component {
             content: 'Learn how to incorporate Robot Operating System into your projects.',
             image: `${baseUrl}img/feature_ros.png`,
             imageAlign: 'top',
-            imageLink: `${baseUrl}docs/tutorials/ros-tutorials/1-ros-introduction`,
+            imageLink: `${baseUrl}tutorials/ros-tutorials/1-ros-introduction`,
             title: 'ROS Tutorials',
           },
           {
             content: 'Learn how to use Husarion products like CORE2 and ROSbot.',
             image: `${baseUrl}img/feature_manuals.png`,
             imageAlign: 'top',
-            imageLink: `${baseUrl}docs/manuals`,
+            imageLink: `${baseUrl}manuals`,
             title: 'Hardware Manuals',
           },
           {
@@ -207,6 +207,7 @@ class Index extends React.Component {
         <div className="mainContainer">
         <Features />
         </div>
+        <script src="https://static.husarion.com/js/qa.js" async></script>
       </div>
     );
   }
